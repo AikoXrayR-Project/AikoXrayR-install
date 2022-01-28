7e11ddf558f9a2d8612d112d92d95770d1367bce
Aiko <aikocute@icloud.com>
2022-01-28 14:14:27 +0700

    Update

--- README.md
+++ README.md
@@ -3,59 +3,35 @@ Một khung phụ trợ Xray có thể dễ dàng hỗ trợ nhiều bảng.
 
 Một khung công tác back-end dựa trên Xray, hỗ trợ các giao thức V2ay, Trojan, Shadowsocks, cực kỳ dễ dàng mở rộng và hỗ trợ kết nối nhiều bảng điều khiển
 
-Tìm mã nguồn tại đây: [AikoCute/XrayR](https://github.com/AikoCute/XrayR)
+Tìm mã nguồn tại đây: [XrayR-project/XrayR](https://github.com/XrayR-project/XrayR)
 
 # Hướng dẫn chi tiết
 
-[hướng dẫn](https://AikoCute.gitbook.io/aikoxrayr/) < Tiếng Việt>
+[hướng dẫn](https://xrayr.aikocute.com/)
 
 # Một cài đặt chính
 
 ```
-bash <(curl -Ls https://raw.githubusercontent.com/AikoCute/XrayR-release/install/install.sh)
+bash <(curl -Ls https://raw.githubusercontent.com/XrayR-project/XrayR-release/master/install.sh)
 ```
 # Cài đặt Docker
 
 ```
-sudo apt-get update
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-sudo apt-get install docker-ce docker-ce-cli containerd.io -y
-systemctl start docker
-systemctl enable docker
+docker pull crackair/xrayr:latest && docker run --restart=always --name xrayr -d -v ${PATH_TO_CONFIG}/config.yml:/etc/XrayR/config.yml --network=host crackair/xrayr:latest
 ```
 
 # Cài đặt Docker-compile
-0. Cài Đặt docker-compose: 
+0. 安装docker-compose: 
 ```
 curl -fsSL https://get.docker.com | bash -s docker
 curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 ```
-1. tải XrayR về VPS để cài đặt
- ```
- git clone https://github.com/AikoCute/XrayR-release
- ```
-2. Truy cập vào thư mục vừa tải
- ```
- cd XrayR-release
- ```
-3. Chỉnh sửa cấu hình.
-Định dạng cơ bản của tệp cấu hình như sau. Nhiều bảng và nhiều thông tin cấu hình nút có thể được thêm vào cùng một lúc trong Nodes, chỉ cần thêm các mục Nodes ở cùng một định dạng.
-4. bắt đầu docker： 
-```
-docker-compose up -d
-```
-File Config Của XrayR cho sever Việt Nam < AikoCuteHotMe>
+1. `git clone https://github.com/XrayR-project/XrayR-release`
+2. `cd XrayR-release`
+3. 编辑config。
+配置文件基本格式如下，Nodes下可以同时添加多个面板，多个节点配置信息，只需添加相同格式的Nodes item即可。
+4. 启动docker：`docker-compose up -d`
 ```
 Log:
   Level: none # Log level: none, error, warning, info, debug 
@@ -72,11 +48,11 @@ ConnetionConfig:
   BufferSize: 64 # The internal cache size of each connection, kB 
 Nodes:
   -
-    PanelType: "V2board" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
+    PanelType: "SSpanel" # Panel type: SSpanel, V2board, PMpanel, Proxypanel
     ApiConfig:
-      ApiHost: "https://aikocute.com"
-      ApiKey: "adminadminadminadminadmin"
-      NodeID: 1
+      ApiHost: "http://127.0.0.1:667"
+      ApiKey: "123"
+      NodeID: 41
       NodeType: V2ray # Node type: V2ray, Trojan, Shadowsocks, Shadowsocks-Plugin
       Timeout: 30 # Timeout for the api request
       EnableVless: false # Enable Vless for V2ray Type
@@ -93,7 +69,7 @@ Nodes:
       DisableUploadTraffic: false # Disable Upload Traffic to the panel
       DisableGetRule: false # Disable Get Rule from the panel
       DisableIVCheck: false # Disable the anti-reply protection for Shadowsocks
-      DisableSniffing: true # Disable domain sniffing 
+      DisableSniffing: false # Disable domain sniffing 
       EnableProxyProtocol: false # Only works for WebSocket and TCP
       EnableFallback: false # Only support for Trojan and Vless
       FallBackConfigs:  # Support multiple fallbacks
@@ -106,7 +82,7 @@ Nodes:
         CertMode: dns # Option about how to get certificate: none, file, http, dns. Choose "none" will forcedly disable the tls config.
         CertDomain: "node1.test.com" # Domain to cert
         CertFile: /etc/XrayR/cert/node1.test.com.cert # Provided if the CertMode is file
-        KeyFile: /etc/XrayR/cert/node1.test.com.key
+KeyFile: /etc/XrayR/cert/node1.test.com.key
         Provider: alidns # DNS cert provider, Get the full support list here: https://go-acme.github.io/lego/dns/
         Email: test@me.com
         DNSEnv: # DNS ENV option used by DNS provider
---
+++ XrayR.sh
@@ -0,0 +1,484 @@
+#!/bin/bash
+
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+plain='\033[0m'
+
+version="v1.0.0"
+
+# check root
+[[ $EUID -ne 0 ]] && echo -e "${red}Lỗi: ${plain} Tập lệnh này phải được chạy với tư cách người dùng root!\n" && exit 1
+
+# check os
+if [[ -f /etc/redhat-release ]]; then
+    release="centos"
+elif cat /etc/issue | grep -Eqi "debian"; then
+    release="debian"
+elif cat /etc/issue | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+elif cat /proc/version | grep -Eqi "debian"; then
+    release="debian"
+elif cat /proc/version | grep -Eqi "ubuntu"; then
+    release="ubuntu"
+elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+    release="centos"
+else
+    echo -e "${red}Phiên bản hệ thống không được phát hiện, vui lòng liên hệ với tác giả kịch bản！${plain}\n" && exit 1
+fi
+
+os_version=""
+
+# os version
+if [[ -f /etc/os-release ]]; then
+    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
+fi
+if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
+    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
+fi
+
+if [[ x"${release}" == x"centos" ]]; then
+    if [[ ${os_version} -le 6 ]]; then
+        echo -e "${red}Vui lòng sử dụng CentOS 7 trở lên！${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"ubuntu" ]]; then
+    if [[ ${os_version} -lt 16 ]]; then
+        echo -e "${red}Vui lòng sử dụng Ubuntu 16 trở lên！${plain}\n" && exit 1
+    fi
+elif [[ x"${release}" == x"debian" ]]; then
+    if [[ ${os_version} -lt 8 ]]; then
+        echo -e "${red}Vui lòng sử dụng Debian 8 trở lên！${plain}\n" && exit 1
+    fi
+fi
+
+confirm() {
+    if [[ $# > 1 ]]; then
+        echo && read -p "$1 [vỡ nợ$2]: " temp
+        if [[ x"${temp}" == x"" ]]; then
+            temp=$2
+        fi
+    else
+        read -p "$1 [y/n]: " temp
+    fi
+    if [[ x"${temp}" == x"y" || x"${temp}" == x"Y" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+confirm_restart() {
+    confirm "Có khởi động lại XrayR không" "y"
+    if [[ $? == 0 ]]; then
+        restart
+    else
+        show_menu
+    fi
+}
+
+before_show_menu() {
+    echo && echo -n -e "${yellow}Nhấn enter để quay lại menu chính: ${plain}" && read temp
+    show_menu
+}
+
+install() {
+    bash <(curl -Ls https://raw.githubusercontent.com/herotbty/Aiko-XrayR/install/install.sh)
+    if [[ $? == 0 ]]; then
+        if [[ $# == 0 ]]; then
+            start
+        else
+            start 0
+        fi
+    fi
+}
+
+update() {
+    if [[ $# == 0 ]]; then
+        echo && echo -n -e "Nhập phiên bản được chỉ định (phiên bản mới nhất mặc định): " && read version
+    else
+        version=$2
+    fi
+#    confirm "Chức năng này sẽ buộc cài đặt lại phiên bản mới nhất và dữ liệu sẽ không bị mất. Bạn có muốn tiếp tục không?" "n"
+#    if [[ $? != 0 ]]; then
+#        echo -e "${red}Đã hủy${plain}"
+#        if [[ $1 != 0 ]]; then
+#            before_show_menu
+#        fi
+#        return 0
+#    fi
+    bash <(curl -Ls https://raw.githubusercontent.com/herotbty/Aiko-XrayR/install/install.sh) $version
+    if [[ $? == 0 ]]; then
+        echo -e "${green}Cập nhật hoàn tất, XrayR đã được khởi động lại tự động, vui lòng sử dụng nhật ký XrayR để xem nhật ký chạy${plain}"
+        exit
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+config() {
+    echo "XrayR sẽ tự động khởi động lại sau khi sửa đổi cấu hình"
+    vi /etc/XrayR/config.yml
+    sleep 2
+    check_status
+    case $? in
+        0)
+            echo -e "Trạng thái XrayR: ${green}đã được chạy${plain}"
+            ;;
+        1)
+            echo -e "Nó được phát hiện rằng bạn không khởi động XrayR hoặc XrayR không tự khởi động lại, hãy kiểm tra nhật ký？[Y/n]" && echo
+            read -e -p "(vỡ nợ: y):" yn
+            [[ -z ${yn} ]] && yn="y"
+            if [[ ${yn} == [Yy] ]]; then
+               show_log
+            fi
+            ;;
+        2)
+            echo -e "Trạng thái XrayR: ${red}Chưa cài đặt${plain}"
+    esac
+}
+
+uninstall() {
+    confirm "Bạn có chắc chắn muốn gỡ cài đặt XrayR không?" "n"
+    if [[ $? != 0 ]]; then
+        if [[ $# == 0 ]]; then
+            show_menu
+        fi
+        return 0
+    fi
+    systemctl stop XrayR
+    systemctl disable XrayR
+    rm /etc/systemd/system/XrayR.service -f
+    systemctl daemon-reload
+    systemctl reset-failed
+    rm /etc/XrayR/ -rf
+    rm /usr/local/XrayR/ -rf
+
+    echo ""
+    echo -e "Gỡ cài đặt thành công, nếu bạn muốn xóa tập lệnh này, hãy chạy sau khi thoát tập lệnh ${green}rm /usr/bin/XrayR -f${plain} xóa"
+    echo ""
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+start() {
+    check_status
+    if [[ $? == 0 ]]; then
+        echo ""
+        echo -e "${green}XrayR đã chạy rồi, không cần khởi động lại, nếu muốn khởi động lại, vui lòng chọn khởi động lại${plain}"
+    else
+        systemctl start XrayR
+        sleep 2
+        check_status
+        if [[ $? == 0 ]]; then
+            echo -e "${green}XrayR đã khởi động thành công, vui lòng sử dụng nhật ký XrayR để xem nhật ký đang chạy${plain}"
+        else
+            echo -e "${red}XrayR có thể không khởi động được, vui lòng sử dụng nhật ký XrayR để xem thông tin nhật ký sau này${plain}"
+        fi
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+stop() {
+    systemctl stop XrayR
+    sleep 2
+    check_status
+    if [[ $? == 1 ]]; then
+        echo -e "${green}XrayR đã dừng thành công${plain}"
+    else
+        echo -e "${red}XrayR không dừng được, có thể do thời gian dừng vượt quá hai giây, vui lòng kiểm tra thông tin nhật ký sau${plain}"
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+restart() {
+    systemctl restart XrayR
+    sleep 2
+    check_status
+    if [[ $? == 0 ]]; then
+        echo -e "${green}XrayR đã khởi động lại thành công, vui lòng sử dụng nhật ký XrayR để xem nhật ký đang chạy${plain}"
+    else
+        echo -e "${red}XrayR có thể không khởi động được, vui lòng sử dụng nhật ký XrayR để xem thông tin nhật ký sau này${plain}"
+    fi
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+status() {
+    systemctl status XrayR --no-pager -l
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+enable() {
+    systemctl enable XrayR
+    if [[ $? == 0 ]]; then
+        echo -e "${green}XrayR được thiết lập để khởi động thành công${plain}"
+    else
+        echo -e "${red}Thiết lập XrayR không thể tự động khởi động khi khởi động${plain}"
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+disable() {
+    systemctl disable XrayR
+    if [[ $? == 0 ]]; then
+        echo -e "${green}XrayR đã hủy khởi động tự động khởi động thành công${plain}"
+    else
+        echo -e "${red}XrayR không thể hủy tự động khởi động khởi động${plain}"
+    fi
+
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+show_log() {
+    journalctl -u XrayR.service -e --no-pager -f
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+install_bbr() {
+    bash <(curl -L -s https://raw.githubusercontent.com/herotbty/Aiko-bbr-Crack/master/tcp.sh)
+    #if [[ $? == 0 ]]; then
+    #    echo ""
+    #    echo -e "${green}Quá trình cài đặt bbr thành công, vui lòng khởi động lại máy chủ${plain}"
+    #else
+    #    echo ""
+    #    echo -e "${red}Không thể tải xuống tập lệnh cài đặt bbr, vui lòng kiểm tra xem máy tính của bạn có thể kết nối với Github không${plain}"
+    #fi
+
+    #before_show_menu
+}
+
+update_shell() {
+    wget -O /usr/bin/XrayR -N --no-check-certificate https://raw.githubusercontent.com/herotbty/Aiko-XrayR/data/XrayR.sh
+    if [[ $? != 0 ]]; then
+        echo ""
+        echo -e "${red}Không tải được script xuống, vui lòng kiểm tra xem máy có thể kết nối với Github không${plain}"
+        before_show_menu
+    else
+        chmod +x /usr/bin/XrayR
+        echo -e "${green}Tập lệnh nâng cấp thành công, vui lòng chạy lại tập lệnh${plain}" && exit 0
+    fi
+}
+
+# 0: running, 1: not running, 2: not installed
+check_status() {
+    if [[ ! -f /etc/systemd/system/XrayR.service ]]; then
+        return 2
+    fi
+    temp=$(systemctl status XrayR | grep Active | awk '{print $3}' | cut -d "(" -f2 | cut -d ")" -f1)
+    if [[ x"${temp}" == x"running" ]]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
+check_enabled() {
+    temp=$(systemctl is-enabled XrayR)
+    if [[ x"${temp}" == x"enabled" ]]; then
+        return 0
+    else
+        return 1;
+    fi
+}
+
+check_uninstall() {
+    check_status
+    if [[ $? != 2 ]]; then
+        echo ""
+        echo -e "${red}XrayR đã được cài đặt, vui lòng không cài đặt lại${plain}"
+        if [[ $# == 0 ]]; then
+            before_show_menu
+        fi
+        return 1
+    else
+        return 0
+    fi
+}
+
+check_install() {
+    check_status
+    if [[ $? == 2 ]]; then
+        echo ""
+        echo -e "${red}Vui lòng cài đặt XrayR trước${plain}"
+        if [[ $# == 0 ]]; then
+            before_show_menu
+        fi
+        return 1
+    else
+        return 0
+    fi
+}
+
+show_status() {
+    check_status
+    case $? in
+        0)
+            echo -e "Trạng thái XrayR: ${green}đã được chạy${plain}"
+            show_enable_status
+            ;;
+        1)
+            echo -e "Trạng thái XrayR: ${yellow}không chạy${plain}"
+            show_enable_status
+            ;;
+        2)
+            echo -e "Trạng thái XrayR: ${red}Chưa cài đặt${plain}"
+    esac
+}
+
+show_enable_status() {
+    check_enabled
+    if [[ $? == 0 ]]; then
+        echo -e "Có tự động bắt đầu không: ${green}Đúng${plain}"
+    else
+        echo -e "Có tự động bắt đầu không: ${red}Không${plain}"
+    fi
+}
+
+show_XrayR_version() {
+    echo -n "Phiên bản XrayR："
+    /usr/local/XrayR/XrayR -version
+    echo ""
+    if [[ $# == 0 ]]; then
+        before_show_menu
+    fi
+}
+
+show_usage() {
+    echo "XrayR Cách sử dụng tập lệnh quản lý (tương thích với thực thi xrayr, không phân biệt chữ hoa chữ thường): "
+    echo "Aiko-XrayR Supported Zalo And fix Vmess"
+    echo "------------------------------------------"
+    echo "  XrayR                    - Hiển thị menu quản lý (nhiều chức năng hơn)"
+    echo "  XrayR start              - Khởi động XrayR"
+    echo "  XrayR stop               - Dừng XrayR"
+    echo "  XrayR restart            - Khởi động lại XrayR"
+    echo "  XrayR status             - Kiểm tra trạng thái XrayR"
+    echo "  XrayR enable             - Kích hoạt XrayR"
+    echo "  XrayR disable            - Hủy tự động khởi động XrayR"
+    echo "  XrayR log                - Xem nhật ký XrayR"
+    echo "  XrayR update             - Cập nhật XrayR"
+    echo "  XrayR update x.x.x       - Cập nhật phiên bản được chỉ định XrayR"
+    echo "  XrayR config             - Hiển thị nội dung tệp cấu hình"
+    echo "  XrayR install            - Cài đặt XrayR"
+    echo "  XrayR uninstall          - Gỡ cài đặt XrayR"
+    echo "  XrayR version            - Xem các phiên bản XrayR"
+    echo "  AikoCute Hotme           - Lệnh Này méo có đâu nên đừng sài"
+    echo "------------------------------------------"
+}
+
+show_menu() {
+    echo -e "
+  ${green}XrayR Các tập lệnh quản lý phụ trợ，${plain}${red}không hoạt động với docker${plain}
+--- https://github.com/XrayR-project/XrayR ---
+  ${green}0.${plain} Thay đổi cài đặt
+————————————————
+  ${green}1.${plain} Cài đặt XrayR
+  ${green}2.${plain} Cập nhật XrayR
+  ${green}3.${plain} Gỡ cài đặt XrayR
+————————————————
+  ${green}4.${plain} Khởi động XrayR
+  ${green}5.${plain} Dừng XrayR
+  ${green}6.${plain} Khởi động lại XrayR
+  ${green}7.${plain} Xem trạng thái XrayR
+  ${green}8.${plain} Xem nhật ký XrayR
+————————————————
+  ${green}9.${plain} Đặt XrayR để bắt đầu tự động
+ ${green}10.${plain} Hủy tự động khởi động XrayR
+————————————————
+ ${green}11.${plain} Một cú nhấp chuột cài đặt bbr (hạt nhân mới nhất)
+ ${green}12.${plain} Xem các phiên bản XrayR 
+ ${green}13.${plain} Nâng cấp Tập lệnh Bảo trì
+ "
+ # Cập nhật tiếp theo có thể được thêm vào chuỗi trên
+    show_status
+    echo && read -p "Vui lòng nhập một lựa chọn [0-13]: " num
+
+    case "${num}" in
+        0) config
+        ;;
+        1) check_uninstall && install
+        ;;
+        2) check_install && update
+        ;;
+        3) check_install && uninstall
+        ;;
+        4) check_install && start
+        ;;
+        5) check_install && stop
+        ;;
+        6) check_install && restart
+        ;;
+        7) check_install && status
+        ;;
+        8) check_install && show_log
+        ;;
+        9) check_install && enable
+        ;;
+        10) check_install && disable
+        ;;
+        11) install_bbr
+        ;;
+        12) check_install && show_XrayR_version
+        ;;
+        13) update_shell
+        ;;
+        *) echo -e "${red}Vui lòng nhập số chính xác [0-12]${plain}"
+        ;;
+    esac
+}
+
+
+if [[ $# > 0 ]]; then
+    case $1 in
+        "start") check_install 0 && start 0
+        ;;
+        "stop") check_install 0 && stop 0
+        ;;
+        "restart") check_install 0 && restart 0
+        ;;
+        "status") check_install 0 && status 0
+        ;;
+        "enable") check_install 0 && enable 0
+        ;;
+        "disable") check_install 0 && disable 0
+        ;;
+        "log") check_install 0 && show_log 0
+        ;;
+        "update") check_install 0 && update 0 $2
+        ;;
+        "config") config $*
+        ;;
+        "install") check_uninstall 0 && install 0
+        ;;
+        "uninstall") check_install 0 && uninstall 0
+        ;;
+        "version") check_install 0 && show_XrayR_version 0
+        ;;
+        "update_shell") update_shell
+        ;;
+        *) show_usage
+    esac
+else
+    show_menu
+fi
