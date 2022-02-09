8a915369270b5985cbd2d5e40d372bd0136dea35
Aiko <aikocute@icloud.com>
2022-02-10 00:54:04 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-git clone https://github.com/AikoCute/XrayR-release
+suijishu=`cat /dev/urandom | tr -dc '0-9a-z' | head -c 15`
+git clone https://github.com/AikoCute/XrayR-release /tmp/$suijishu && cd /tmp/$suijishu
 sleep 5s
-file="./XrayR-release/config/config.yml"
+file="./config.yml"
 link="https://aikocute.com/"
 docker_compose_file="./docker-compose.yml"
 #Nhập thông tin
-read -p "Vui lòng nhập tên vùng chứa < Aiko >：" docker_name
+read -p "Vui lòng nhập tên vùng chứa：" docker_name
 read -p "Vui lòng nhập địa chỉ bảng điều khiển <API Host > ：" apihost
 read -p "Vui lòng nhập API Key Web："  apikey
 read -p "Vui lòng nhập ID nút：" nodeid
