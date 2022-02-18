7aa3cd3a67c4c3caac44ebe8b92001b09714689f
Aiko <aikocute@icloud.com>
2022-02-18 20:02:25 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
+
+echo "${green}bắt đầu cài đặt docker${plain}"
+sudo apt-get update
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg-agent \
+    software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+systemctl start docker
+systemctl enable docker
+echo "${green}đã cài đặt docker-compose${plain}"
+curl -fsSL https://get.docker.com | bash -s docker
+curl -L "https://github.com/docker/compose/releases/download/1.26.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+echo "Cài đặt config file"
 suijishu=`cat /dev/urandom | tr -dc '0-9a-z' | head -c 15`
 git clone https://github.com/AikoCute/XrayR-release /tmp/$suijishu && cd /tmp/$suijishu
 sleep 5s
