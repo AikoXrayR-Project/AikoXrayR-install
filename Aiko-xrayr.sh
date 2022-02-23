04cc7fcbe1fdba991e09b4715d7a0444c927f362
Aiko <91626055+AikoCute@users.noreply.github.com>
2022-02-23 21:37:19 +0700

    Update Aiko-xrayr.sh

--- Aiko-xrayr.sh
+++ Aiko-xrayr.sh
@@ -183,7 +183,7 @@ config_docker() {
 version: '3'
 services: 
   xrayr: 
-    image: aikocute/aikoxrayr:Latest
+    image: aikocute/xrayr:latest
     volumes:
       - ./config.yml:/etc/XrayR/config.yml # thư mục cấu hình bản đồ
       - ./dns.json:/etc/XrayR/dns.json 
