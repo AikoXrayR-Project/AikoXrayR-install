fccad845bc28f3aa2688f3534b67f2a6b186424d
AikoCute-Offical <aikocute@icloud.com>
2022-06-19 14:18:26 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -171,7 +171,7 @@ install_XrayR() {
     if [[ ! -f /etc/XrayR/custom_outbound.json ]]; then
         cp custom_outbound.json /etc/XrayR/
     fi
-    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/AikoCute/AikoXrayR-install/master/XrayR.sh
+    curl -o /usr/bin/XrayR -Ls https://raw.githubusercontent.com/AikoCute/AikoXrayR-install/dev/XrayR.sh
     chmod +x /usr/bin/XrayR
     ln -s /usr/bin/XrayR /usr/bin/xrayr # chữ thường tương thích
     chmod +x /usr/bin/xrayr
